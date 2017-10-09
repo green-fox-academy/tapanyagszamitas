2aad87dcab38bb784b3f493bfd96f913e3816e6f
Peter Jolankai <tapanyagszamitas.gmail.com>
2017-10-09 17:35:04 +0200

    factorio isnot ready

---
+++ week-02/day-01/03greet/greenfox.h
@@ -0,0 +1,4 @@
+#define greeting "Greenfox"
+#define name1 "Jozsika"
+#define name2 "Pistike"
+#define name3 "Marika"
---
+++ week-02/day-01/04_factorio/Factorio/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "par_for_factorio.h"
+
+// create a function called `factorio`
+// that returns it's input's factorial with and without recursion
+// again the parameters value should be stored in a .h file
+//
+
+int main()
+{
+
+    return 0;
+}
+
+void factorio(int number1)
+{
+ int result = 1;
+
+    for (int i = 1; i <= n; i++) {
+        result = result * i;
+
+}
