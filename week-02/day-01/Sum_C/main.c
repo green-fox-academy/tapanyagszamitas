ee4484ad9dc6b8d93e24d2fa0363bb4442c0beb1
Peter Jolankai <tapanyagszamitas.gmail.com>
2017-10-09 16:00:03 +0200

    another files are ready

---
+++ week-02/day-01/Sum_C/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "parameter.h"
+int sum(int num1, int num2, int num3, int num4);
+
+
+// Write a function called `sum` that sum all the numbers
+// until the given parameter which you store in a .h file
+
+int main()
+{
+
+printf( "%d\n", sum(a, b, c, d));
+
+    return 0;
+}
+
+int sum(int num1, int num2, int num3, int num4)
+{
+    return num1 + num2 + num3 + num4;
+}
---
+++ week-02/day-01/Sum_C/parameter.h
@@ -0,0 +1,4 @@
+int a = 1;
+int b = 3;
+int c = 4;
+int d = 5;
