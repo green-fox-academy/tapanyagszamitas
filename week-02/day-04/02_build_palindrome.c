97f1c7ee2af89b264ffb4a2197cac7be19d87dd8
Peter Jolankai <tapanyagszamitas.gmail.com>
2017-10-12 15:37:54 +0200

    palindrome one is ready

---
+++ week-02/day-04/02_build_palindrome.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+char *create_palindrom(char word1[]);
+
+int main()
+{
+   char arr[100];
+
+    printf("enter word\n");
+    scanf("%s", arr);
+
+
+    int i;
+    i = strlen(arr);
+
+     printf("Here is your Conjoined twins style palindrom\n");
+
+    for(i=0; i<=(strlen(arr))-1; i++){
+    printf("%c", arr[i]);
+    }
+
+    for(i=(strlen(arr))-1; i>=0; i--){
+    printf("%c", arr[i]);
+    }
+
+    return;
+}
