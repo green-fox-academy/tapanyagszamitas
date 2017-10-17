8edbac3b8574b27515782985dae79a1d0400cd6e
Peter Jolankai <tapanyagszamitas.gmail.com>
2017-10-17 13:09:51 +0200

    ready with the 6th one

---
+++ week-03/day-02/05_transforming.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+//TODO: write a function which returns 485 as unsigned integer value
+//put your return value in an integer variable in which you should have 229, and print it out
+//try to figure out which type casting you should use
+
+
+int main()
+{
+
+int a = 485;
+unsigned int b = 0;
+
+b = (unsigned int)a;
+
+printf(" %d", b);
+
+
+    return 0;
+}
