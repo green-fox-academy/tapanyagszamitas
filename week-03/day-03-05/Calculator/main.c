43b9160c78ba80b0eb0e516fa7f0a1fa8e4f0dd4
Peter Jolankai <tapanyagszamitas.gmail.com>
2017-10-18 15:00:46 +0200

    calculating calculator

--- week-03/day-03-05/Calculator/main.c
+++ week-03/day-03-05/Calculator/main.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+int print_welcome_screen()
+
+int main()
+{
+
+print_welcome_screen;
+  return 0;
+}
+
+int print_welcome_screen
+{
+
+
+ printf("\t The Boring Calculator\n"
+        "====================================\n"
+        "usage: [number] [operation] [number]\n"
+        "Commands:\n"
+        "\n"
+        " + \tsummation\n"
+        " * \tmultiplication\n"
+        " / \tdivision\n"
+        " % \tdivision with remainder\n"
+        " ^ \tsquaring\n"
+        " < \tsquare root\n"
+        " log \tlogarithm\n"
+        " binto \tbinary to hex or dec\n"
+        " hexto \thexadecimal to bin or dec\n"
+        " decto \tdecimal to bin or hex\n"
+        "====================================\n"
+        "exit \texiting from the program\n"
+        "clear \tclear the screen\n"
+        "help \tprint usage\n"
+        "====================================\n"
+        "Hit enter to start!\n"
+        "====================================\n");
+
+return main;
+}
+
+
 
