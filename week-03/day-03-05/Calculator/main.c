8d18edfd259433347cc02436f0b94768eb9e2a90
Peter Jolankai <tapanyagszamitas.gmail.com>
2017-10-19 11:36:02 +0200

    snake calculator in action

--- week-03/day-03-05/Calculator/main.c
+++ week-03/day-03-05/Calculator/main.c
@@ -9,15 +9,34 @@ char enter = 0;
 while (enter != '\r' && enter != '\n') { enter = getchar(); }
 printf("Please enter your commands!\n");
 
-char collect_data[100] = " ";
+char firstnumber[10] = " ";
+char opperator [10] = " ";
+char lastnumber[10] = " ";
 
-scanf( "%s %s %s", &collect_data[1], &collect_data[40], &collect_data[60]);
+
+scanf( "%s", &firstnumber[1]);
+scanf( "%s", &opperator[1]);
+scanf( "%s", &lastnumber[1]);
 
 
+printf("firstnumber\n");
     int i;
-    for(i=0; i<=99; i++){
-    printf("%c\n", collect_data[i]);
+    for(i=0; i<=9; i++){
+    printf("%c\n", firstnumber[i]);
     }
+printf("opperator\n");
+     int i2;
+    for(i2=0; i2<=9; i2++){
+    printf("%c\n", opperator[i2]);
+    }
+printf("lastnumber\n");
+     int i3;
+    for(i3=0; i3<=9; i3++){
+    printf("%c\n", lastnumber[i3]);
+    }
+
+
+
 
 
 
