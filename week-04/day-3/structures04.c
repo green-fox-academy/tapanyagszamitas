5ca3792243a422af2e53088c3b51eeecaa0b69a4
Peter Jolankai <tapanyagszamitas.gmail.com>
2017-10-25 15:32:51 +0200

    cars ready

---
+++ week-04/day-3/structures04.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+void getsurface();
+
+struct rectangular_cuboid {
+    double a;
+    double b;
+    double c;
+};
+
+void getsurface(rectangular_cuboid){
+
+
+
+
+
+
+}
+// TODO:
+// Write a function called "GetSurface" that takes a RectangularCuboid
+// and returns it's surface
+
+// TODO:
+// Write a function called "GetVolume" that takes a RectangularCuboid
+// and returns it's volume
+
+int main()
+{
+
+
+
+
+
+    return 0;
+}
