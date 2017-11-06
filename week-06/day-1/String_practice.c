b9df6d8b7c2cfa5d22e14d3383cc009f43bfb1e9
tapanyagszamitas <tapanyagszamitas@gmail.com>
2017-11-06 10:24:49 +0100

    monday starting with structures practice

---
+++ week-06/day-1/String_practice.c
@@ -0,0 +1,12 @@
+//TODO:
+//Create a structure called "student":
+//name of the student
+//number of the student's grade - error handling
+//type of the student's behavior in an enumeration ("behaviors"): - BAD_BEHAVIOR , GOOD_BEHAVIOR
+//The student's are stored in an array.
+// Write a function that returns with the grade's avarage
+// write a function that prints out if the student's grade higher than 3 and the behavior is bad:
+//"(student's name) you are fuckin amazing, please behave good"
+//write a function that counts good behaviors, if they have 5 grades:
+//return with the count of students
+
