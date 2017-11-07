948533236b486c8debaba81b6f3ba9878653b206
tapanyagszamitas <tapanyagszamitas@gmail.com>
2017-11-07 19:38:36 +0100

    05.c houses is ready

--- week-06/day-2/05_houses.c
+++ week-06/day-2/05_houses.c
@@ -17,30 +17,47 @@
 // Create a function that takes an array of houses (and it's length), and counts the
 // houses that are worth to buy.
 
-struct house{
-char adress[200];
-int price;
-int rooms;
-int area;
-}house_alias;
+struct house
+{
+    char adress[200];
+    int price;
+    int rooms;
+    int area;
+} house_alias;
 
-int main() {
+char *houses_good_price(struct house houses[], int marketprice);
 
-struct house houses[]={
-{"First street 1", 3000, 4, 100},
-{"Second street 2", 4000, 5, 100},
-{"Third street 3", 5000, 6, 200},
-{"Forth street 4", 6000, 6, 300},
-{"Fifth street 5", 7000, 5, 250}
-};
+int main()
+{
+    int marketprice = 400;
 
+    struct house houses[] =
+    {
+        {"First street 1", 30000, 4, 50},
+        {"Second street 2", 40000, 5, 110},
+        {"Third street 3", 50000, 6, 200},
+        {"Forth street 4", 60000, 6, 325},
+        {"Fifth street 5", 70000, 5, 150}
+    };
 
+    for(int i =0; i <=4; i++){
+        printf("\n %s \t%d \t%d \t%d", houses[i].adress,houses[i].price, houses[i].rooms, houses[i].area);
+    }
 
+    houses_good_price(houses, marketprice);
 
-
-
-
-
-  return 0;
+    return 0;
 }
 
+char *houses_good_price(struct house houses[], int marketprice)
+{
+    int number_of_houses = 5;
+    int count = 0;
+    for(int i = 0; i <= number_of_houses-1; i++){
+        if((houses[i].price / houses[i].area ) <= marketprice){
+            printf("\n\nit's a good buy : %s \t%d \t%d \t%d", houses[i].adress,houses[i].price, houses[i].rooms, houses[i].area);
+            count ++;
+        }
+    }
+    printf("\n\nThe number of good priced houses is (are) = %d\n", count);
+}
