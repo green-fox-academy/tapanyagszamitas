b31f42cfac6bcb08db0b686236622113f898cb84
Peter Jolankai <tapanyagszamitas.gmail.com>
2017-11-13 16:00:22 +0100

    namespace 02 dir is ready

---
+++ week-07 C++/day-01/namespace02/namespace02.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#include "person.h"
+
+// Create your own namespace, called Person
+// store your name as a string
+// your age as an integer
+// and your favorite number as an integer
+// write them out in main()
+
+int main()
+{
+
+person :: display();
+    cout << person ::name;
+
+    return 0;
+}
