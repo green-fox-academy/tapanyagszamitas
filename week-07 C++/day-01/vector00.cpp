4161ee82ba53d4df9baf95b7a41f7a3a02931b57
Peter Jolankai <tapanyagszamitas.gmail.com>
2017-11-13 17:52:24 +0100

    ready for today

--- week-07 C++/day-01/vector00.cpp
+++ week-07 C++/day-01/vector00.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 //create an integer vector with the size of 5 and print the 3rd element of it
 
 int main() {
+
+
 
 
   return 0;
