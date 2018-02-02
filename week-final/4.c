971c6ba92d1919ba6313ff46096b3226db47b104
Peter Jolankai <tapanyagszamitas.gmail.com>
2018-02-02 10:30:56 +0100

    example ready

--- week-final/4.c
+++ week-final/4.c
@@ -8,30 +8,30 @@
 
 int main()
 {
-int array_length = 20;
-char array[array_length] = {NULL};
+char array[20] = {NULL};
 strcpy(array, "abcdABCD012340000");
 
 int count_of_capitals = 0;
 int count_of_lowercase = 0;
 int count_of_numbers = 0;
 
-    for(int i=0; i<=array_length-1; i++){
-    if (array[i] >= 'A' && array[i] <= 'Z')
-        count_of_capitals ++;
-    else if (array[i] >= 'a' && array[i] <= 'z')
-        count_of_lowercase  ++;
-    else if (array[i] >= '0' && array[i] <= '9')
-        count_of_numbers ++;
-
-    printf("%c", array[i]);
+    for(int i=0; i < sizeof(array) - 1; i++){
+        if (array[i] >= 'A' && array[i] <= 'Z')
+            count_of_capitals ++;
+        else if (array[i] >= 'a' && array[i] <= 'z')
+            count_of_lowercase  ++;
+        else if (array[i] >= '0' && array[i] <= '9')
+            count_of_numbers ++;
+        printf("%c", array[i]); //test print array
     }
 
-printf( "\nIn this array the number of characters is: %d \n\n",count_of_capitals + count_of_lowercase);
-printf( "In this array the number of capital letters is: %d \n", count_of_capitals);
-printf( "In this array the number of lowercase letters is: %d \n", count_of_lowercase);
-printf( "In this array the number of numeric elements is: %d \n", count_of_numbers);
+printf( "\nIn this array the number of all elements: %d \n\n",count_of_capitals + count_of_lowercase + count_of_numbers);
+printf( "\nIn this array the number of characters: %d \n\n",count_of_capitals + count_of_lowercase);
+printf( "In this array the number of capital letters: %d \n", count_of_capitals);
+printf( "In this array the number of lowercase letters: %d \n", count_of_lowercase);
+printf( "In this array the number of numeric elements: %d \n", count_of_numbers);
 
+return 0;
 }
 
 
